3022e46a7df942869b6ad9c18661f50a1407b449
Abhishek Khati <abhishekkhati39@gmail.com>
2025-04-24 23:16:21 +0545

    feat: Refactor login form to include card component and improve layout
    delete: Remove unused NavDocuments and NavSecondary components
    
    fix: Update NavMain to include ModeToggle and adjust button properties
    
    refactor: Simplify NavUser component and enhance user avatar display
    
    style: Clean up imports and formatting in various components
    
    feat: Add NotFound page with user-friendly message and navigation
    
    feat: Implement ModeToggle for theme switching with dropdown menu
    
    feat: Create ThemeProvider for managing theme context in the application

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
 
-import { IconCirclePlusFilled, IconMail, type Icon } from "@tabler/icons-react"
+import { type Icon } from "@tabler/icons-react";
 
-import { Button } from "@/components/ui/button"
 import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
+import { ModeToggle } from "./mode-toggle";
 
 export function NavMain({
   items,
 }: {
   items: {
-    title: string
-    url: string
-    icon?: Icon
-  }[]
+    title: string;
+    url: string;
+    icon?: Icon;
+  }[];
 }) {
   return (
     <SidebarGroup>
@@ -26,20 +26,12 @@ export function NavMain({
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
             <SidebarMenuButton
-              tooltip="Quick Create"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+              tooltip="visitors"
+              className="bg-primary text-center text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
             >
-              <IconCirclePlusFilled />
-              <span>Quick Create</span>
+              <span className="w-full">Visitors</span>
             </SidebarMenuButton>
-            <Button
-              size="icon"
-              className="size-8 group-data-[collapsible=icon]:opacity-0"
-              variant="outline"
-            >
-              <IconMail />
-              <span className="sr-only">Inbox</span>
-            </Button>
+            <ModeToggle />
           </SidebarMenuItem>
         </SidebarMenu>
         <SidebarMenu>
@@ -54,5 +46,5 @@ export function NavMain({
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
-  )
+  );
 }
